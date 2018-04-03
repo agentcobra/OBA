06dee23d437c0dd1c8d18799488abab30d665562
Simon Cosandey <simon@cosandey.ch>
2018-04-03 12:58:50 +0200

    first commit with directory structure

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomepageController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('homepage/index.html.twig');
+    }
+}
